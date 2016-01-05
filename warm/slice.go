312854ac91f0c8025470b6ece128ff5887538968
wangzhf <972695371@qq.com>
2016-01-05 11:14:51 +0800

    add nil test.

--- warm/slice.go
+++ warm/slice.go
@@ -11,7 +11,7 @@ import (
 //	4. []Type{value1, value2, ..., valueN} ：复合语法，其长度为大括号内项的个数
 
 //slice 是一个数据结构，其指向一个数组某个连续的部分。slice 用起来很像数组。[]T 为 slice 类型，其中元素类型为 T
-func testSclice() {
+func testSlice() {
 	p := []int{2, 3, 5, 11}
 	fmt.Println("p == ", p)
 
@@ -21,7 +21,7 @@ func testSclice() {
 }
 
 //截取切片
-func testSclice2() {
+func testSlice2() {
 	s := []string{"A", "B", "C", "D", "E", "F", "G"}
 	t := s[:5]
 	u := s[3 : len(s)-1]
@@ -33,7 +33,7 @@ func testSclice2() {
 }
 
 // make([]Type, length, capacity) ：其长度即为切片的容量
-func testMakeSclice1(){
+func testMakeSlice1(){
 	a := make([]string, 0, 5) //length必须小于等于capacity
 	fmt.Println("a : ", a)
 	fmt.Println("len : ", len(a))
@@ -41,7 +41,7 @@ func testMakeSclice1(){
 }
 
 // make([]Type, length) ：其长度即为切片的长度
-func testMakeSclice2(){
+func testMakeSlice2(){
 	b := make([]string, 0)
 	fmt.Println(b)
 	fmt.Println("len : ", len(b))
@@ -49,7 +49,7 @@ func testMakeSclice2(){
 }
 
 // []Type{} ：复合语法，其长度为大括号内项的个数
-func testMakeSclice3(){
+func testMakeSlice3(){
 	c := []string{}
 	fmt.Println(c)
 	fmt.Println("len : ", len(c)) // out is 0
@@ -57,20 +57,40 @@ func testMakeSclice3(){
 }
 
 // []Type{value1, value2, ..., valueN} ：复合语法，其长度和容量为大括号内项的个数
-func testMakeSclice4(){
+func testMakeSlice4(){
 	d := []string{"Beijing", "New York", "Las Vergas", "California"}
 	fmt.Println(d)
 	fmt.Println("len : ", len(d))
 	fmt.Println("cap : ", cap(d))
 }
 
+//测试nil
+func testNilSlice(){
+	var e []int //e is nil
+	fmt.Println(e, len(e), cap(e))
+	if e == nil {
+		fmt.Println("e is Nil")
+	}else {
+		fmt.Println("e is not Nil")
+	}
+	
+	f := []int{}  //f is not nil
+	fmt.Println(f, len(f), cap(f))
+	if f == nil {
+		fmt.Println("f is Nil")
+	}else{
+		fmt.Println("f is not Nil.")
+	}
+}
+
 /*
 func main() {
-	//testSclice()
-	//testSclice2()
-	//testMakeSclice1()
-	//testMakeSclice2()
-	//testMakeSclice3()
-	testMakeSclice4()
+	//testSlice()
+	//testSlice2()
+	//testMakeSlice1()
+	//testMakeSlice2()
+	//testMakeSlice3()
+	//testMakeSlice4()
+	testNilSlice()
 }
 */
