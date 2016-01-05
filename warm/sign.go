2fc4e1812d64baa36adf74bc09c74fb9deed61c2
wangzhf <972695371@qq.com>
2016-01-05 17:39:11 +0800

    add the & and * test

---
+++ warm/sign.go
@@ -0,0 +1,31 @@
+package main	
+
+import (
+	"fmt"
+)
+
+//区分符号&与*
+func testIdentify(){
+	var a int = 1 
+    var b *int = &a
+    var c **int = &b
+    var x int = *b
+    fmt.Println("a = ",a)
+    fmt.Println("&a = ",&a)
+    fmt.Println("*&a = ",*&a)
+    fmt.Println("b = ",b)
+    fmt.Println("&b = ",&b)
+    fmt.Println("*&b = ",*&b)
+    fmt.Println("*b = ",*b)
+    fmt.Println("c = ",c)
+    fmt.Println("*c = ",*c)
+    fmt.Println("&c = ",&c)
+    fmt.Println("*&c = ",*&c)
+    fmt.Println("**c = ",**c)
+    fmt.Println("***&*&*&*&c = ",***&*&*&*&*&c)
+    fmt.Println("x = ",x)
+}
+
+func main() {
+    
+}
