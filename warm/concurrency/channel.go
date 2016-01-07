9b05facc40bc833c5d48e173a5078a1fca7b7290
wangzhf <972695371@qq.com>
2016-01-07 21:36:20 +0800

    添加channel理解

--- warm/concurrency/channel.go
+++ warm/concurrency/channel.go
@@ -4,11 +4,26 @@ import (
 	"fmt"
 )
 
+//发送和接收的语法：
+// channel <- value     //阻塞发送
+// <- channel           //接收并将其丢弃
+// x := <- channel      //接收并用x保存
+// x, ok := <- channel  //功能同上，同时检查通道是否关闭或者是否为空
+
+//通道可以使用内置的make()函数通过以下语法来创建：
+// make(chan Type)
+// make(chan Type, capacity)  //capacity为缓冲容量
+
 func createCounter(start int) chan int {
+	//该通道是同步的，因此会阻塞直到发送者准备好发送 或 接收者准备接收
+	//由于通道创建时容量为0，因此该发送回阻塞直到收到一个从通道中接收数据的请求
+	//该阻塞只会影响到此例中的匿名函数所在的goroutine
 	next := make(chan int)
 	go func(i int) {
 		for {
+			fmt.Println("i: ", i)
 			next <- i
+			fmt.Println("get data from channel.")
 			i++
 		}
 	}(start)
