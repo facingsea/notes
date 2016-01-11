d2e5ed55b10a17a4398f5f42a9fdc597e552d93f
wangzhf <972695371@qq.com>
2016-01-11 22:55:41 +0800

    从os中获取main方法输入参数

---
+++ warm/os/mainInputVal.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+//init方法先于main方法执行
+func init() {
+	fmt.Println("init method run.")
+}
+
+//测试main方法获取输入参数
+//用命令行的方式执行：go run mainInputVal.go "zhangsan"
+func testMain() {
+	who := "World!"
+	fmt.Println(os.Args[0]) // out is E:\code\coding.net\golang\warm\os\os.exe，为改程序编译的名称
+	if len(os.Args) > 1 {
+		who = strings.Join(os.Args[1:], " ")
+	}
+	fmt.Println("Hello ", who)
+}
+
+/*
+func main() {
+	testMain()
+}
+*/
