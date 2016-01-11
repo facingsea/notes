8a3630c23cc6459218bc369344addc97d90e727c
wangzhf <972695371@qq.com>
2016-01-11 21:08:55 +0800

    测试方法内初始化新结构体

--- warm/interface/emptyInterface.go
+++ warm/interface/emptyInterface.go
@@ -6,7 +6,7 @@ import "fmt"
 func testDeclear() {
 	var v1 interface{} = 1
 	var v2 interface{} = "hello"
-	var v3 interface{} = struct { X int }{ 1 }
+	var v3 interface{} = struct{ X int }{1}
 	fmt.Println(v1)
 	fmt.Println(v2)
 	fmt.Println(v3)
@@ -14,17 +14,21 @@ func testDeclear() {
 
 //如果函数打算接收任何数据类型，则可以将参考声明为interface{}。
 //最典型的例子就是标准库fmt包中的Print和Fprint系列的函数
-func testInterface(){
-	var user struct{Name string; Gender int}
-    user.Name = "dotcoo"
+func testInterface() {
+	var user struct {
+		Name   string
+		Gender int
+	}
+	user.Name = "dotcoo"
 	myPrintln(user)
 	myPrintln("hello", " ", 25, user)
 	//测试匿名结构体
-	myPrintln(struct { X int }{ 1 })
+	myPrintln(struct{ X int }{1})
+	//myPrintln(struct{X int; Y int,}{1; 2,}) //无法使用
 }
 
 //
-func myPrintln(a ...interface{}){
+func myPrintln(a ...interface{}) {
 	fmt.Println(a)
 }
 
