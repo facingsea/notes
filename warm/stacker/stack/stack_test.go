6140516092149a96351f35160a2aed9cda49e4d5
wangzhf <972695371@qq.com>
2016-01-13 22:41:52 +0800

    add stacker to test stack

---
+++ warm/stacker/stack/stack_test.go
@@ -0,0 +1,24 @@
+package stack_test
+
+import (
+	"stack"
+	"testing"
+)
+
+func TestStack(t *testing.T) {
+	count := 1
+	var myStack stack.Stack
+	assertTrue(t, myStack.Len() == 0, "expected empty stack.", count)
+}
+
+func assertTrue(t *testing.T, condition bool, message string, id int) {
+	if !condition {
+		t.Errorf("#%d: %s", id, message)
+	}
+}
+
+func assertEqualString(t *testing.T, a, b, string, message string, id int) {
+	if a != b {
+		t.Errorf("#%d: %s \"%s\" != \n\"%s\" ", id, message, a, b)
+	}
+}
