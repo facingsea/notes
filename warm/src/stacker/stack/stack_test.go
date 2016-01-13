6339523d0262a39ec76b25481399bb447a0cefb7
wangzhf <972695371@qq.com>
2016-01-13 23:07:01 +0800

    test stack

--- warm/src/stacker/stack/stack_test.go
+++ warm/src/stacker/stack/stack_test.go
@@ -1,7 +1,7 @@
 package stack_test
 
 import (
-	"stack"
+	"stacker/stack"
 	"testing"
 )
 
@@ -9,6 +9,8 @@ func TestStack(t *testing.T) {
 	count := 1
 	var myStack stack.Stack
 	assertTrue(t, myStack.Len() == 0, "expected empty stack.", count)
+	count++
+	assertTrue(t, myStack.Cap() == 0, "expected empty stack.", count)
 }
 
 func assertTrue(t *testing.T, condition bool, message string, id int) {
