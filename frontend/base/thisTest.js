6846068eb96531770ff482e074dcf7524fc1f3b3
wangzhf <972695371@qq.com>
2016-03-04 22:14:03 +0800

    js中this测试

---
+++ frontend/base/thisTest.js
@@ -0,0 +1,55 @@
+// 情况一：纯粹的函数调用
+// 当前指定的是全局的window对象
+function test(){
+	this.x = 1;
+	console.log(this); // windows
+}
+
+test();
+
+//==================================
+
+var x = 1;
+
+function test2(){
+	this.x = 0;
+}
+
+test2();
+console.log(x); // 0
+
+//==================================
+//情况二：作为对象方法的调用
+
+function test3(){
+	console.log(this.x);
+}
+var o = {};
+o.x = 3;
+o.f = test3;
+o.f(); // this指向o，输出为 3
+
+
+//==================================
+// 情况三 作为构造函数调用
+
+function test4(){
+	this.x = 4;
+}
+var t4 = new test4();
+console.log(t4.x); // 当前this指向t4，输出 4
+
+
+//==================================
+// 情况四 apply调用
+
+var x5 = 5;
+function test5(){
+	console.log(this.x5);
+}
+var t5 = {};
+t5.x5 = 55;
+t5.f = test5;
+t5.f.apply(); // apply()的参数为空时，默认调用全局对象。因此，这时的运行结果为 5 ，证明this指的是全局对象。
+
+t5.f.apply(t5); // 运行结果就变成了 55 ，证明了这时this代表的是对象t5。
