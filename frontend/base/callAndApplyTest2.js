f2a073ea5ef3a9013c4b7ee122d83512addf9b21
wangzhf <972695371@qq.com>
2016-03-04 23:34:33 +0800

    添加个人理解

--- frontend/base/callAndApplyTest2.js
+++ frontend/base/callAndApplyTest2.js
@@ -15,6 +15,7 @@ var animal = new Animal();
 var cat = new Cat();
 
 // 相当于把animal.showName中的this换成了cat，this.name ==> cat.name
+// 也可以理解为：cat继承了animal，也就有了showName方法，从而执行的是cat.showName()
 animal.showName.call(cat);   //输出 Cat
 // 调用者必须是function
 animal.name.call(cat); // Uncaught TypeError: animal.name.call is not a function
