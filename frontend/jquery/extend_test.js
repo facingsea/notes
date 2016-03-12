b097c013c1a4d3cc132c8bdbc23d2fb9a30ad537
wangzhf <972695371@qq.com>
2016-03-12 23:16:30 +0800

    添加jQuery测试分类

---
+++ frontend/jquery/extend_test.js
@@ -0,0 +1,15 @@
+/**
+ * 测试jQuery的 $.extend()方法
+ */
+
+/**
+ *	Jquery的扩展方法原型是:
+ *		extend(dest,src1,src2,src3...);
+ *	它的含义是将src1,src2,src3...合并到dest中,返回值为合并后的dest,
+ *	由此可以看出该方法合并后，是修改了dest的结构的。如果想要得到合并的结果却又不想修改dest的结构，可以如下使用：
+ * 		var newSrc=$.extend({},src1,src2,src3...) //也就是将"{}"作为dest参数。
+ */
+
+
+
+
